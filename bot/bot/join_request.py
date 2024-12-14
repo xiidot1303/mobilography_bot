57fbf1744c6718e7dc5047b3ea8746487e2e973f
xiidot1303 <xii1303@inbox.ru>
2024-12-14 19:49:22 +0500

    exception if bot user didnot find

--- bot/bot/join_request.py
+++ bot/bot/join_request.py
@@ -2,10 +2,13 @@ from bot.bot import *
 
 
 async def channel_join_request(update: Update, context: CustomContext):
-    # check user already has access to channel
-    bot_user: Bot_user = await get_object_by_update(update)
-    if bot_user.has_access_to_channel:
-        await update.chat_join_request.approve()
-        text = context.words.successfully_joined
-        await context.bot.send_message(context._user_id, text)
-        return
+    try:
+        # check user already has access to channel
+        bot_user: Bot_user = await get_object_by_update(update)
+        if bot_user.has_access_to_channel:
+            await update.chat_join_request.approve()
+            text = context.words.successfully_joined
+            await context.bot.send_message(context._user_id, text)
+            return
+    except:
+        None
